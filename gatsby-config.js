1282a8a07dcfd4fdb40335186bfbd083a91838ac
jongjin-j <jjjung3571@gmail.com>
2021-07-16 22:42:05 -0400

    Handling images using gatsby plugin image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,27 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    'gatsby-transformer-remark',
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      }
+    },
+  ],
   siteMetadata: {
-    title: 'Portfolio'
+    title: 'Portfolio',
+    contact: 'jjjung3571@gmail.com'
   }
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,59 @@
+import { graphql, Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
 
-export default function Projects() {
+
+export default function Projects({ data }) {
+
+    const projects = data.projects.nodes
+    const contact = data.contact.siteMetadata.contact
+
+    console.log(projects[0].frontmatter.projectImage)
+
     return (
         <Layout>
             <div className={styles.portfolio}>
                 <h2>Personal Projects</h2>
+                <div className={styles.projects}>
+                    {projects.map(p => (
+                        <Link to={"/projects/" + p.frontmatter.slug} key = {p.id}>
+                            <div>
+                                <StaticImage src={p.frontmatter.projectImage} alt="Google Maps"/>
+                                <h3>{p.frontmatter.title}</h3>
+                                <p>{p.frontmatter.stack}</p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+
+                <p>Email: {contact}</p>
             </div>
         </Layout>
             
     )
 }
+
+export const query = graphql`
+    query projectQuery {
+        projects: allMarkdownRemark {
+            nodes {
+                frontmatter {
+                    title
+                    stack
+                    slug
+                    projectImage
+                }
+                id
+            }
+        }
+        contact: site {
+            siteMetadata {
+                contact
+            }
+        }
+    }
+`
+
+/*<StaticImage src="../../images/GoogleMaps.png" alt="Google Maps"/>*/
